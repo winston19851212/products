1a42931a8c0cb975543c7df7e042c705c9da0a59
Wen Chiang <vanishman@gmail.com>
2021-07-07 21:54:27 +0800

    add how to find the file

--- products.py
+++ products.py
@@ -1,4 +1,22 @@
+import os
+
+if os.path.isfile('products.csv'):
+	print('yeah! I found the file!')
+else:
+	print('I can not find the file....')
+	
+
 products = []
+with open ('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if 'product, price' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+
+
 while True:
     name = input('please inout the name of products: ')
     if name == 'q':
@@ -7,4 +25,13 @@ while True:
     products.append([name, price])
 
 print (products)
-products[0][0]
+
+for p in products:
+	print(p[0], " 's price is ", p[1])
+
+with open ('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('product, price \n')
+	for p in products:
+		f.write(p[0] + ', ' + p[1] + '\n')
+
+
